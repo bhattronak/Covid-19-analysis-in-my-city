853caa155514ee17b7f3caafa2c507606d96b72e
Ronak Bhatt <73295127+bhattronak@users.noreply.github.com>
2020-10-23 16:17:47 +0530

    Create covidanalysis.py

---
+++ covidanalysis.py
@@ -0,0 +1,2 @@
+# Covid-19-analysis-in-my-city
+   I have taken data of number of cases for daily basis from website. for covid 19 analysis. I have played around those data and made some useful graphs.You can also do it for your city,state as well as country by importing this code.
